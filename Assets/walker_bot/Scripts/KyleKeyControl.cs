5208f5a20c6c883f8ed6ccaf5462db88f84e08f2
RyodoTanaka <groadpg@gmail.com>
2020-08-10 18:58:11 +0900

    First commit

---
+++ Assets/walker_bot/Scripts/KyleKeyControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KyleKeyControl : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        bool Stop = true;
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Stop = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            Stop = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Stop = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow)) {
+            Stop = true;
+        }
+        else {
+            Stop = false;
+        }
+        GetComponent<Animator>().SetBool("Stop",Stop);  
+    }
+}
